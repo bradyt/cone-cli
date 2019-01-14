30be8b4da83380364cc5440d5dc5100d62baa639
Brady Trainor <mail@bradyt.com>
2019-01-14 15:28:27 -0800

    Clean up grammar

--- lib/src/parser/grammar.dart
+++ lib/src/parser/grammar.dart
@@ -8,12 +8,12 @@ class LedgerGrammarDefinition extends GrammarDefinition {
   Parser journal() => ref(journalItem).star();
   Parser journalItem() => ref(journalItemWhitespace) | ref(transaction);
 
-  Parser journalItemWhitespace() => ref(newline) | ref(comment) & ref(newline);
+  Parser journalItemWhitespace() => ref(newline) | ref(note) & ref(newline);
 
   Parser transaction() =>
       ref(date) &
-      (char('=') & ref(date)).optional() &
-      whitespace().star() &
+      ref(effectiveDate).optional() &
+      char(' ').star() &
       ref(status).optional() &
       ref(description).optional() &
       ref(note).optional() &
@@ -23,26 +23,26 @@ class LedgerGrammarDefinition extends GrammarDefinition {
   Parser date() => ref(dateComponent).separatedBy(ref(dateSep));
   Parser dateComponent() => digit().plus();
   Parser dateSep() => anyOf('/-.');
+  Parser effectiveDate() => char('=') & ref(date);
   Parser status() => anyOf('!*');
   Parser description() => any().starLazy(char(';') | ref(newline));
-  Parser note() => ref(comment);
 
   Parser postings() => ref(posting).star();
   Parser posting() =>
-      whitespace().plus() &
-      (ref(status) & whitespace().plus()).optional() &
+      char(' ').plus() &
+      ref(status).optional() &
       ref(account) &
-      (whitespace().repeat(2, unbounded) & ref(amount)).optional() &
+      ref(values).optional() &
       ref(note).optional() &
       ref(newline);
 
-  Parser account() => any()
-      .starLazy(whitespace().repeat(2, unbounded) | char(';') | ref(newline));
-  Parser amount() => any().starLazy(char(';') | ref(newline));
+  Parser account() =>
+      any().starLazy(char(' ').times(2) | char(';') | ref(newline));
+  Parser values() =>
+      char(' ').times(2) & any().starLazy(char(';') | ref(newline));
 
-  Parser whitespace() => pattern(' \t');
   Parser newline() => Token.newlineParser();
-  Parser comment() => char(';') & any().starLazy(ref(newline));
+  Parser note() => char(';') & any().starLazy(ref(newline));
 }
 
 class LedgerGrammar extends GrammarParser {
